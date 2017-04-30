8c571516bdc31c4eb58df562b2aa769ba00c6854
chepinoga <chepinoga.s@gmail.com>
2017-04-30 15:25:10 +0300

    commitmessage

---
+++ view/indexTemplate.php
@@ -0,0 +1,10 @@
+<form method="post" action="index.php">
+    <input type="text" placeholder="name" name="name">
+    </br>
+    <input type="text" placeholder="description" name="description">
+    </br>
+    <input type="date" placeholder="created_at" name="created_at">
+    </br>
+    <input type="submit">
+</form>
+
